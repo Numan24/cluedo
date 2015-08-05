1ec8e5d663cc67f6b8f2aefa54623e82bed9bdcd
JasenPalmer <jasen_palmer@hotmail.com>
2015-08-05 18:33:15 +1200

    added end class

---
+++ action/End.java
@@ -0,0 +1,27 @@
+package cluedo.action;
+
+import cluedo.Game;
+import cluedo.Player;
+
+public class End extends Action {
+
+	public End(Game game, Player player) {
+		super(game, player);
+	}
+
+	@Override
+	public boolean isValid() {
+		return player.getRoll() > 0;
+	}
+
+	@Override
+	public void run() {
+		return;
+	}
+
+	@Override
+	public boolean endsTurn() {
+		return true;
+	}
+
+}
