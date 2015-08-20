6c68463de70756f11bea0560dd392e66fef96c23
JasenPalmer <jasen_palmer@hotmail.com>
2015-08-21 10:12:17 +1200

    changed outline

--- gui/boardPanel.java
+++ gui/boardPanel.java
@@ -2,9 +2,9 @@ package cluedo.gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
@@ -12,7 +12,6 @@ import javax.swing.border.LineBorder;
 import cluedo.Board;
 import cluedo.Game;
 import cluedo.Player;
-import cluedo.tile.DoorTile;
 import cluedo.tile.FloorTile;
 import cluedo.tile.RoomTile;
 import cluedo.tile.Tile;
@@ -41,10 +40,12 @@ public class boardPanel extends JPanel {
 			for(int j = 0; j < Board.BOARD_LENGTH; j++) {
 				JPanel tile = new JPanel(); {
 					tile.setPreferredSize(new Dimension(TILE_SIZE,TILE_SIZE));
-					tile.setSize(TILE_SIZE, TILE_SIZE);
-					tile.setBorder(new LineBorder(Color.BLACK));
 					tile.setBackground(board[i][j].getColor());
-					tile.setForeground(board[i][j].getColor());
+					//tile.setForeground(board[i][j].getColor());
+					if(board[i][j] instanceof RoomTile) {
+						calculateBorder(tile, i, j);
+					}
+					else {tile.setBorder(new LineBorder(Color.BLACK));}
 					add(tile);
 				}
 			}
@@ -52,85 +53,36 @@ public class boardPanel extends JPanel {
 		
 		setBorder(new LineBorder(Color.black));
 	}
-	
-	
 
-	public void paint(Graphics g){
 
-		players = game.getPlayerPositions();
-		int x = 0;
-		int y = 0;
-		
-		g.setColor(Color.WHITE);
-		g.fillRect(x, y, board[0].length*TILE_SIZE, board.length*TILE_SIZE);
-		
-		// THE BOARD TILES
-		for(int i = 0; i < board.length; i++){
-			for(int j = 0; j < board[0].length; j++){
-				if(board[i][j] instanceof FloorTile){
-					g.setColor(Color.GRAY);
-					floor.paintIcon(this, g, x, y);
-				} 
-				else if(board[i][j] instanceof DoorTile){
-					door.paintIcon(this, g, x, y);
-				}
-				else{					
-					room.paintIcon(this, g, x, y);
-				}
-				
-				if(players[i][j]!=null){
-					g.setColor(players[i][j].getColor());
-					g.fillOval(x, y, TILE_SIZE, TILE_SIZE);
-					g.setColor(Color.BLACK);
-					g.drawOval(x,y, TILE_SIZE, TILE_SIZE);
-				}
-				
-				x+=TILE_SIZE;
+	private void calculateBorder(JPanel tile, int y, int x) {
+		Tile[][] board  = game.getBoardArray();
+		int top = 0;
+		int right = 0;
+		int bottom = 0;
+		int left = 0;
+		if(x < 23){
+			if(board[y][x+1] instanceof FloorTile) {
+				right = 1;
 			}
-			x=0;
-			y+=TILE_SIZE;
-		}
-		
-		
-		// Did I really do all this just for outlines
-		x=0;
-		y=0;
-		g.setColor(Color.BLACK);
-		for(int i = 0; i < board.length; i++){
-			for(int j = 0; j < board[0].length; j++){
-				if(board[i][j] instanceof RoomTile){
-					if(i+1!=board.length){
-						if(board[i+1][j] instanceof FloorTile){							
-							g.drawLine(x, y+TILE_SIZE, x+TILE_SIZE, y+TILE_SIZE);
-						}
-					}
-					
-					if(i-1>0){
-						if(board[i-1][j] instanceof FloorTile){
-							g.drawLine(x, y, x+TILE_SIZE, y);
-						}
-					}
-					
-					if(j+1!=board[0].length){
-					if(board[i][j+1] instanceof FloorTile){
-						g.drawLine(x+TILE_SIZE, y, x+TILE_SIZE, y+TILE_SIZE);
-					}
-					}
-					
-					if(j-1>0){
-						if(board[i][j-1] instanceof FloorTile){
-							g.drawLine(x, y, x, y+TILE_SIZE);
-						}
-					}
-				}
-				x+=TILE_SIZE;
+		} else{right = 1;}
+		if(x > 0){
+			if(board[y][x-1] instanceof FloorTile) {
+				left = 1;
 			}
-			x=0;
-			y+=TILE_SIZE;
-		}
-
-		
-		g.drawRect(0, 0, board[0].length*TILE_SIZE, board.length*TILE_SIZE);
+		} else{left = 1;}
+		if(y < 24){
+			if(board[y+1][x] instanceof FloorTile) {
+				bottom = 1;
+			}
+		} else {bottom = 1;}
+		if(y > 0){
+			if(board[y-1][x] instanceof FloorTile) {
+				top = 1;
+			}
+		}else {top = 1;}
+		tile.setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, Color.black));
 	}
+	
 
 }
